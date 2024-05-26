a66b6e0d21beff393f89b4aa1e4e8f3cd46aa99d
Thoriq Aziz <aziz.thoriq9@gmail.com>
2024-05-26 08:16:03 +0700

    add internationalization and versioning by url

--- src/main/java/com/thoriqaziz/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/thoriqaziz/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,10 +1,19 @@
 package com.thoriqaziz.rest.webservices.restful_web_services.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
 
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
@@ -18,5 +27,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
---
+++ src/main/java/com/thoriqaziz/rest/webservices/restful_web_services/versioning/PersonV1.java
@@ -0,0 +1,20 @@
+package com.thoriqaziz.rest.webservices.restful_web_services.versioning;
+
+public class PersonV1 {
+    private String name;
+
+    public PersonV1(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonV1{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+}
