2ac0e74c609593888c8a605ed83f286916b2ae85
Thoriq Aziz <aziz.thoriq9@gmail.com>
2024-05-26 17:47:23 +0700

    use JSONProperty and Filtering

--- src/main/java/com/thoriqaziz/rest/webservices/restful_web_services/user/User.java
+++ src/main/java/com/thoriqaziz/rest/webservices/restful_web_services/user/User.java
@@ -1,5 +1,6 @@
 package com.thoriqaziz.rest.webservices.restful_web_services.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -9,9 +10,11 @@ public class User {
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
+    @JsonProperty("user_name")
     private String name;
 
     @Past(message = "BirthDate should be in the past")
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
