7d21fb6ea8274d03293fbae0ebc6bdbc3a5beb64
Thoriq Aziz <aziz.thoriq9@gmail.com>
2024-05-26 10:48:05 +0700

    add link to the API Response using EntityModel

--- src/main/java/com/thoriqaziz/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/thoriqaziz/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,6 +1,8 @@
 package com.thoriqaziz.rest.webservices.restful_web_services.user;
 
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -23,12 +25,16 @@ public class UserResource {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public EntityModel<User> retrieveUser(@PathVariable int id){
         User user = userService.findById(id);
         if (user == null)
             throw new UserNotFoundException("id:" + id);
 
-        return user;
+        EntityModel<User> userEntityModel = EntityModel.of(user);
+        WebMvcLinkBuilder webMvcLinkBuilder = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+        userEntityModel.add(webMvcLinkBuilder.withRel("all-users"));
+
+        return userEntityModel;
     }
 
     @DeleteMapping("/users/{id}")
--- src/main/java/com/thoriqaziz/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
+++ src/main/java/com/thoriqaziz/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
@@ -16,4 +16,14 @@ public class VersioningPersonController {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
 
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter(){
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter(){
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
 }
